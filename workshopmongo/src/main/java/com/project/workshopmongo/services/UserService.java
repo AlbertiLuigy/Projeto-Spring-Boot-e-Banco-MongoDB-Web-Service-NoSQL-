dadedba98cb80159c6239ae5d5c5087751ff2372
Alberti Luigy <albertiluigy@gmail.com>
2026-09-23 19:29:48 -0300

    Inserção de usuário com POST

--- workshopmongo/src/main/java/com/project/workshopmongo/services/UserService.java
+++ workshopmongo/src/main/java/com/project/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.project.workshopmongo.domain.User;
+import com.project.workshopmongo.dto.UserDTO;
 import com.project.workshopmongo.repository.UserRepository;
 import com.project.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -16,12 +17,20 @@ public class UserService {
     @Autowired 
     private UserRepository repo;
 
-    public List<User> findAll(){
+    public List<User> findAll(){ //findAll() é um método que retorna uma lista de todos os usuários do banco de dados. Ele utiliza o repositório UserRepository para buscar todos os usuários e retorná-los como uma lista.
         return repo.findAll();
     }
 
     public User findById(String id){
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado")); 
+    }
+
+    public User insert(User obj){ //função insert() é um método que insere um novo usuário no banco de dados. Ele recebe um objeto User como parâmetro e utiliza o repositório UserRepository para salvar o usuário no banco de dados.
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objdto){
+            return new User(objdto.getId(), objdto.getName(), objdto.getEmail());
     }
 }
